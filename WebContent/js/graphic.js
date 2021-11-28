098f68100ab708b1a3d8656ba68a1a4cb15bafae
kalelfleith <kalelfleith03@gmail.com>
2021-11-28 20:49:33 -0300

    Update 23#

--- WebContent/js/graphic.js
+++ WebContent/js/graphic.js
@@ -26,24 +26,37 @@ $(document).ready(function(){
 	        	label : ["Temperatura Ambiente"],
 	        	data: [],
 	            backgroundColor: [
-	            	'rgba(135,206,235)'		                
+	            	'rgba(32,178,170)'		                
 	            ],
 	            borderColor: [
-	            	'rgba(135,206,235)'	                
+	            	'rgba(32,178,170)'	                
 	            ],
 	            borderWidth: 1
 	        },
 	        {
 	        	label : ["Predição"],
 	        	data: [],
+	        	pointRadius: 0,
 	            backgroundColor: [
 	            	'rgba(255,69,0)'
 	            ],
 	            borderColor: [	                   
 	                'rgba(255,69,0)'
 	            ],
-	            borderWidth: 1,
+	            borderWidth: 2,
 	            borderDash: [5,5]
+	        },
+	        {
+	        	label : ["Temperatura Limite"],
+	        	data: [],
+	        	pointRadius: 0,
+	            backgroundColor: [
+	            	'rgba(255,0,0)'
+	            ],
+	            borderColor: [	                   
+	                'rgba(255,0,0)'
+	            ],
+	            borderWidth: 1	            
 	        }
 	        ]
 	    },
@@ -97,38 +110,53 @@ $(document).ready(function(){
 	        {
 	        	label : ["Predição 1"],
 	        	data: [],
+	        	pointRadius: 0,
 	            backgroundColor: [
 	                'rgba(255,69,0)'	                
 	            ],
 	            borderColor: [
 	            	'rgba(255,69,0)'                
 	            ],
-	            borderWidth: 1,
+	            borderWidth: 2,
 	            borderDash: [5,5]
 	        },
 	        {
 	        	label : ["Predição 2"],
 	        	data: [],
+	        	pointRadius: 0,
 	            backgroundColor: [
 	                'rgba(255,140,0)'	                
 	            ],
 	            borderColor: [
 	            	'rgba(255,140,0)'                
 	            ],
-	            borderWidth: 1,
+	            borderWidth: 2,
 	            borderDash: [5,5]
 	        },
 	        {
 	        	label : ["Predição 3"],
 	        	data: [],
+	        	pointRadius: 0,
 	            backgroundColor: [
 	                'rgba(255,165,0)'	                
 	            ],
 	            borderColor: [
 	            	'rgba(255,165,0)'	                     
 	            ],
-	            borderWidth: 1,
+	            borderWidth: 2,
 	            borderDash: [5,5]
+	        },
+	        {
+	        	label : ["Corrente Limite"],
+	        	data: [],
+	        	pointRadius: 0,
+	            backgroundColor: [
+	            	'rgba(255,0,0)'
+	            ],
+	            borderColor: [	                   
+	                'rgba(255,0,0)'
+	            ],
+	            borderWidth: 1	            
 	        }
 	        ]
 	    },
@@ -182,38 +210,53 @@ $(document).ready(function(){
 	        {
 	        	label : ["Predição 1"],
 	        	data: [],
+	        	pointRadius: 0,
 	            backgroundColor: [
 	                'rgba(255,69,0)'	                
 	            ],
 	            borderColor: [
 	            	'rgba(255,69,0)'                
 	            ],
-	            borderWidth: 1,
+	            borderWidth: 2,
 	            borderDash: [5,5]
 	        },
 	        {
 	        	label : ["Predição 2"],
 	        	data: [],
+	        	pointRadius: 0,
 	            backgroundColor: [
 	                'rgba(255,140,0)'	                
 	            ],
 	            borderColor: [
 	            	'rgba(255,140,0)'                
 	            ],
-	            borderWidth: 1,
+	            borderWidth: 2,
 	            borderDash: [5,5]
 	        },
 	        {
 	        	label : ["Predição 3"],
 	        	data: [],
+	        	pointRadius: 0,
 	            backgroundColor: [
 	                'rgba(255,165,0)'	                
 	            ],
 	            borderColor: [
 	            	'rgba(255,165,0)'	                     
 	            ],
-	            borderWidth: 1,
+	            borderWidth: 2,
 	            borderDash: [5,5]
+	        },
+	        {
+	        	label : ["Tensão Limite"],
+	        	data: [],
+	        	pointRadius: 0,
+	            backgroundColor: [
+	            	'rgba(255,0,0)'
+	            ],
+	            borderColor: [	                   
+	                'rgba(255,0,0)'
+	            ],
+	            borderWidth: 1	            
 	        }
 	        ]
 	    },
@@ -237,12 +280,15 @@ $(document).ready(function(){
 				temperatureChart.data.datasets[0].data = [];
 				temperatureChart.data.datasets[1].data = [];
 				temperatureChart.data.datasets[2].data = [];
+				temperatureChart.data.datasets[3].data = [];
 				currentChart.data.datasets[0].data = [];
 				currentChart.data.datasets[1].data = [];
 				currentChart.data.datasets[2].data = [];
+				currentChart.data.datasets[6].data = [];
 				voltageChart.data.datasets[0].data = [];
 				voltageChart.data.datasets[1].data = [];
 				voltageChart.data.datasets[2].data = [];
+				currentChart.data.datasets[6].data = [];
 	
 				temperatureChart.data.labels = [];
 				currentChart.data.labels = [];
@@ -251,16 +297,19 @@ $(document).ready(function(){
 				for (i = 0; i < dataObj.length; i++){
 					temperatureChart.data.datasets[0].data.push(dataObj[i].machineTemp);
 					temperatureChart.data.datasets[1].data.push(dataObj[i].localTemp);
+					temperatureChart.data.datasets[3].data.push('90');
 					temperatureChart.data.labels.push(hours);
 					
 					currentChart.data.datasets[0].data.push(dataObj[i].c1);
 					currentChart.data.datasets[1].data.push(dataObj[i].c2);
 					currentChart.data.datasets[2].data.push(dataObj[i].c3)
+					currentChart.data.datasets[6].data.push('1.2')
 					currentChart.data.labels.push(hours);
 					
 					voltageChart.data.datasets[0].data.push(dataObj[i].t1);
 					voltageChart.data.datasets[1].data.push(dataObj[i].t2);
 					voltageChart.data.datasets[2].data.push(dataObj[i].t3)
+					voltageChart.data.datasets[6].data.push('254')
 					voltageChart.data.labels.push(hours);
 					
 				}
@@ -304,7 +353,8 @@ $(document).ready(function(){
 						temperatureChart.data.datasets[2].data.push(temperatureChart.data.datasets[0].data[x]);						
 					}else{
 						temperatureChart.data.datasets[2].data.push(null);						
-					}							
+					}	
+					temperatureChart.data.datasets[3].data.push('90');
 				}
 				
 				for (i = 0; i < dataObj.length; i++){
@@ -321,7 +371,8 @@ $(document).ready(function(){
 						currentChart.data.datasets[3].data.push(null);
 						currentChart.data.datasets[4].data.push(null);
 						currentChart.data.datasets[5].data.push(null);
-					}					
+					}	
+					currentChart.data.datasets[6].data.push('1.2')
 				} 
 				
 				for (i = 0; i < dataObj.length; i++){
@@ -340,7 +391,8 @@ $(document).ready(function(){
 						voltageChart.data.datasets[3].data.push(null);
 						voltageChart.data.datasets[4].data.push(null);
 						voltageChart.data.datasets[5].data.push(null);
-					}					
+					}		
+					voltageChart.data.datasets[6].data.push('254')
 				} 
 				
 				for (i = 0; i < dataObj.length; i++){
